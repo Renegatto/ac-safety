65d46716ee681101e409bde8a6d87f75d04a4507
Renegatto <edittorr@mail.ru>
2024-10-25 18:45:17 +0300

    Remove redundant type level safety

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -17,7 +17,7 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 module MyLib (someFunc) where
 
-import {-qualified-} Ivory.Language as Ivory
+import Ivory.Language as Ivory
 import Ivory.Language.Pointer (Pointer, KnownConstancy, Nullability(Valid))
 import qualified Ivory.Language.Proc as Proc
 import Data.Void (Void, absurd)
@@ -25,26 +25,24 @@ import Data.Kind (Type)
 import Control.Arrow ((>>>))
 import Unsafe.Coerce (unsafeCoerce)
 import qualified Ivory.Language.Pointer as Pointer
-import Sleepiness (CSleepiness, matchSleepiness, Sleepiness (SleepyT, EnergeticT, InsomniacT, LethargicT), SleepinessLevel)
+import Sleepiness (CSleepiness, matchEnum, Sleepiness (Sleepy, Energetic, Insomniac, Lethargic))
 
 newtype Ms = MkMs { unMs :: Uint64 }
   deriving newtype (IvoryType, IvoryVar)
 
-touch :: forall (sleepiness :: SleepinessLevel) s.
+touch :: forall s.
   Ms ->
   ProcPtr ('[Ms] :-> ()) ->
-  (forall (sl :: SleepinessLevel).
-    ProcPtr ('[Ref s (Stored (CSleepiness sl))] :-> ())
-  ) ->
-  Def ('[Ref s (Stored (CSleepiness sleepiness))] :-> ())
+  ProcPtr ('[Ref s (Stored (CSleepiness))] :-> ()) ->
+  Def ('[Ref s (Stored CSleepiness)] :-> ())
 touch sleepDuration sleepFor toTouch = proc "touch" \state -> body do
   indirect_ toTouch state
   state' <- deref state
-  matchSleepiness @sleepiness state' \case
-    EnergeticT -> retVoid
-    InsomniacT -> retVoid
-    SleepyT -> indirect_ sleepFor sleepDuration
-    LethargicT -> indirect_ sleepFor (MkMs (-1))
+  matchEnum state' \case
+    Energetic -> retVoid
+    Insomniac -> retVoid
+    Sleepy -> indirect_ sleepFor sleepDuration
+    Lethargic -> indirect_ sleepFor (MkMs (-1))
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- src/Sleepiness.hs
+++ src/Sleepiness.hs
@@ -17,74 +17,21 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 module Sleepiness where
 
+import Ivory.Language as Ivory
 
-import {-qualified-} Ivory.Language as Ivory
-import Ivory.Language.Pointer (Pointer, KnownConstancy, Nullability(Valid))
-import qualified Ivory.Language.Proc as Proc
-import Data.Void (Void, absurd)
-import Data.Kind (Type)
-import Control.Arrow ((>>>))
-import Unsafe.Coerce (unsafeCoerce)
-import qualified Ivory.Language.Pointer as Pointer
-
-data Equal a b where
-  EqRefl :: Equal a a
-
-data SleepinessLevel
+data Sleepiness
   = Energetic -- not going to fell asleep until become sleepy
   | Sleepy -- going to fell asleep asap
   | Lethargic -- going fell asleep and never wake up
   | Insomniac -- not ever going to fell asleep
   deriving (Bounded, Enum)
 
-type Forget :: forall k. (k -> Type) -> Type
-data Forget f = forall a. MkForget (f a) 
-
-data Sleepiness s where
-  EnergeticT :: Sleepiness Energetic 
-  SleepyT :: Sleepiness Sleepy 
-  LethargicT :: Sleepiness Lethargic 
-  InsomniacT :: Sleepiness Insomniac 
-
 -- | A C representation of Sleepiness
-newtype CSleepiness a = MkCSleepiness Uint8
+newtype CSleepiness = MkCSleepiness Uint8
   deriving newtype (IvoryType, IvoryVar, IvoryStore, IvoryExpr, IvoryEq, Num)
 
--- | Update CSleepiness and returning the same pointer with updated type
-writeSleepiness :: forall s a b eff.
-  IvoryStore (CSleepiness b) =>
-  Ref s (Stored (CSleepiness a)) ->
-  CSleepiness b ->
-  Ivory eff (Ref s (Stored (CSleepiness b)))
-writeSleepiness (Pointer.Pointer ref) b = do
-  let refB = Pointer.Pointer ref
-  store refB b
-  pure refB
-
-toLevel :: Sleepiness s -> SleepinessLevel
-toLevel = \case
-  EnergeticT -> Energetic 
-  SleepyT -> Sleepy 
-  LethargicT -> Lethargic 
-  InsomniacT -> Insomniac 
-
-fromLevel :: SleepinessLevel -> Forget Sleepiness
-fromLevel = \case
-  Energetic -> MkForget EnergeticT 
-  Sleepy -> MkForget SleepyT 
-  Lethargic -> MkForget LethargicT 
-  Insomniac -> MkForget InsomniacT 
-
-sleepinessToC :: forall s. Sleepiness s -> CSleepiness s
-sleepinessToC = MkCSleepiness . fromInteger . toEnum . fromEnum . toLevel
-
--- | Match the haskell enum represented by some C number (as enum?)
--- Inlines all the branches as nested if-then-else statements
-matchSleepiness :: forall s eff. CSleepiness s -> (Sleepiness s -> Ivory eff ()) -> Ivory eff ()
-matchSleepiness conId cont =
-  matchEnum conId $ fromLevel >>> \(MkForget (sleepiness :: Sleepiness s0)) ->
-    -- This relies completely on the correct pattern matching
-    cont $ unsafeCoerce @(Sleepiness s0) @(Sleepiness s) sleepiness
+sleepinessToC :: Sleepiness -> CSleepiness
+sleepinessToC = MkCSleepiness . fromInteger . toEnum . fromEnum
 
 -- | Match the haskell enum represented by some C number (as enum?)
 -- Inlines all the branches as nested if-then-else statements
@@ -101,74 +48,57 @@ matchEnum conId cont = do
     toConId :: b -> conId
     toConId = fromInteger . toEnum . fromEnum
 
-getEnergized :: forall sleepiness (state :: SleepinessLevel -> Type) s.
-  (forall s0. IvoryType (state s0)) =>
-  (forall s0. IvoryStore (state s0)) =>
-  IvoryStore (state sleepiness) =>
-  (state ~ CSleepiness) =>
-  (Equal sleepiness Insomniac -> Void) -> -- insomniacs can not
-  (Equal sleepiness Lethargic -> Void) -> -- lethargics can not
-  Def ('[Ref s (Stored (state sleepiness))] :-> Ref s (Stored (state Energetic)))
-getEnergized nonInsomniac nonLethargic = proc "getEnergized" \state -> body do
+getEnergized :: forall s.
+  IvoryType CSleepiness =>
+  IvoryStore CSleepiness =>
+  IvoryStore CSleepiness =>
+  Def ('[Ref s (Stored CSleepiness)] :-> ())
+getEnergized = proc "getEnergized" \state -> body do
   csleepiness <- deref state
-  matchSleepiness csleepiness \sleepiness -> case sleepiness of
-    EnergeticT ->
-      ret state
-    SleepyT ->
-      ret =<< writeSleepiness state (sleepinessToC EnergeticT)
-    LethargicT -> absurd $ nonLethargic EqRefl
-    InsomniacT -> absurd $ nonInsomniac EqRefl
+  matchEnum csleepiness \sleepiness -> case sleepiness of
+    Energetic -> retVoid
+    Sleepy -> store state (sleepinessToC Energetic)
+    Lethargic -> retVoid -- lethargics can not
+    Insomniac -> retVoid -- insomniacs can not
 
-getLethargicSleep :: forall sleepiness state s.
-  (forall s0. IvoryType (state s0)) =>
-  (forall s0. IvoryStore (state s0)) =>
-  IvoryStore (state sleepiness) =>
-  (state ~ CSleepiness) =>
-  (Equal sleepiness Insomniac -> Void) -> -- insomniacs can not
-  Def ('[Ref s (Stored (state sleepiness))] :-> Ref s (Stored (state Lethargic)))
-getLethargicSleep nonInsomniac = proc "getLethargicSleep" \state -> body do
+getLethargicSleep :: forall s.
+  IvoryType CSleepiness =>
+  IvoryStore CSleepiness=>
+  IvoryStore CSleepiness =>
+  Def ('[Ref s (Stored CSleepiness)] :-> ())
+getLethargicSleep = proc "getLethargicSleep" \state -> body do
   csleepiness <- deref state
-  matchSleepiness csleepiness \sleepiness -> case sleepiness of
-    EnergeticT ->
-      ret =<< writeSleepiness state (sleepinessToC LethargicT)
-    SleepyT ->
-      ret =<< writeSleepiness state (sleepinessToC LethargicT)
-    LethargicT ->
-      ret state
-    InsomniacT -> absurd $ nonInsomniac EqRefl
+  matchEnum csleepiness \sleepiness -> case sleepiness of
+    Energetic -> store state (sleepinessToC Lethargic)
+    Sleepy -> store state (sleepinessToC Lethargic)
+    Lethargic -> retVoid
+    Insomniac -> retVoid -- insomniacs can not
 
-getInsomnia :: forall sleepiness state s.
-  (forall s0. IvoryType (state s0)) =>
-  (forall s0. IvoryStore (state s0)) =>
-  IvoryStore (state sleepiness) =>
-  (state ~ CSleepiness) =>
-  (Equal sleepiness Lethargic -> Void) -> -- lethargic can not
-  Def ('[Ref s (Stored (state sleepiness))] :-> Ref s (Stored (state Insomniac)))
-getInsomnia nonLethargic = proc "getInsomnia" \state -> body do
+getInsomnia :: forall s.
+  IvoryType CSleepiness =>
+  IvoryStore CSleepiness =>
+  IvoryStore CSleepiness =>
+  Def ('[Ref s (Stored CSleepiness)] :-> ())
+getInsomnia = proc "getInsomnia" \state -> body do
   csleepiness <- deref state
-  matchSleepiness csleepiness \sleepiness -> case sleepiness of
-    EnergeticT ->
-      ret =<< writeSleepiness state (sleepinessToC InsomniacT)
-    SleepyT ->
-      ret =<< writeSleepiness state (sleepinessToC InsomniacT)
-    LethargicT -> absurd $ nonLethargic EqRefl
-    InsomniacT -> ret state
+  matchEnum csleepiness \sleepiness -> case sleepiness of
+    Energetic -> store state (sleepinessToC Insomniac)
+    Sleepy -> store state (sleepinessToC Insomniac)
+    Lethargic -> retVoid -- lethargic can not
+    Insomniac -> retVoid
 
-getSleepy :: forall sleepiness state s.
-  (forall s0. IvoryType (state s0)) =>
-  (forall s0. IvoryStore (state s0)) =>
-  IvoryStore (state sleepiness) =>
-  (state ~ CSleepiness) =>
-  (Equal sleepiness Insomniac -> Void) -> -- insomniacs can not
-  Def ('[Ref s (Stored (state sleepiness))] :-> Ref s (Stored (state Sleepy)))
-getSleepy nonInsomniac = proc "getSleepy" \state -> body do
+getSleepy :: forall s.
+  IvoryType CSleepiness =>
+  IvoryStore CSleepiness =>
+  IvoryStore CSleepiness =>
+ -- (Equal sleepiness Insomniac -> Void) -> -- insomniacs can not
+  Def ('[Ref s (Stored CSleepiness)] :-> ())
+getSleepy = proc "getSleepy" \state -> body do
   csleepiness <- deref state
-  matchSleepiness csleepiness \sleepiness -> case sleepiness of
-    EnergeticT ->
-      ret =<< writeSleepiness state (sleepinessToC SleepyT)
-    SleepyT -> ret state
-    LethargicT -> 
-      ret =<< writeSleepiness state (sleepinessToC SleepyT)
-    InsomniacT -> absurd $ nonInsomniac EqRefl
+  matchEnum csleepiness \sleepiness -> case sleepiness of
+    Energetic -> store state (sleepinessToC Sleepy)
+    Sleepy -> retVoid
+    Lethargic -> store state (sleepinessToC Sleepy)
+    Insomniac -> retVoid
 
 
