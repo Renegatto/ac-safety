267b453e24727a751fdac4de6d2471ef485ad152
Renegatto <edittorr@mail.ru>
2024-11-03 21:07:01 +0300

    Nats

--- src/Packing.hs
+++ src/Packing.hs
@@ -29,26 +29,26 @@
 {-# OPTIONS_GHC -Wall -Werror=Wno-incomplete-patterns #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Packing
-  ( Composite (Single, Prepend)
-  , begin
-  , append
-  -- * Packing/unpacking
-  , pack
-  , unpack
-  -- * Instances
-  , CompositeF (MkCompositeF, unCompositeF)
-  , withComposite
-  -- * Part
-  , Part (MkPart, sizeBits, offset, mask)
-  , part
-  , makePart
-  , takePart
-  )
+  -- ( Composite (Single, Prepend)
+  -- , begin
+  -- , append
+  -- -- * Packing/unpacking
+  -- , pack
+  -- , unpack
+  -- -- * Instances
+  -- , CompositeF (MkCompositeF, unCompositeF)
+  -- , withComposite
+  -- -- * Part
+  -- , Part (MkPart, sizeBits, offset, mask)
+  -- , part
+  -- , makePart
+  -- , takePart
+  -- )
   where
 
 import Prelude hiding (init, fail)
 import Ivory.Language as Ivory
-import GHC.TypeNats (KnownNat, natVal, type (-), type (+), type (<=), Nat)
+import GHC.TypeNats (KnownNat, natVal, type (-), type (+), type (<=))
 import Data.Bits (shiftL, (.&.), shiftR, Bits ((.|.)))
 import Data.Type.Ord (Compare)
 import Data.Functor.Const (Const (Const))
@@ -57,22 +57,53 @@ import Data.Foldable (Foldable(fold))
 import Control.Monad.Identity (Identity(Identity))
 import Communication (Uint4)
 
+type family a :+: b where
+  S a :+: b = S (a :+: b)
+  Z :+: b = b
+
+type family a :-: b where
+  S a :-: S b = (a :-: b)
+  a :-: Z = a
+
+type family N2S n where
+  N2S 0 = Z
+  N2S n = S (N2S (n - 1))  
+
+type family S2N n where
+  S2N Z = 0
+  S2N (S n) = 1 + (S2N n)
+
 data Composite hi lo f xs where
-  Single :: forall size a hi lo f
-    . ( KnownNat hi
-      , KnownNat lo
-      , KnownNat size
-      , hi - lo + 1 ~ size
-      , Compare hi lo ~ 'GT
-      , Compare lo  0 ~ 'GT
-      )
-    => f a
-    -> Composite hi lo f '[ '(size,a)]
+  Single :: forall size plo a f
+    . Nat size
+    -> Nat (S plo)
+    -> f a
+    -> Composite (plo :+: size) (S plo) f '[ '(S plo,a)]
   Prepend
-    :: forall hi1 hi0 lo0 f {x} {y}  xs
-    .  Composite hi1 (hi0 + 1) f '[x]
+    :: forall hi1 hi0 lo0 f {x} {y} xs
+    .  Composite hi1 (S hi0) f '[x]
     -> Composite hi0 lo0 f (y:xs)
     -> Composite hi1 lo0 f (x:y:xs)
+
+apply :: forall f g h hi lo ns.
+  (forall a. f a -> g a -> h a) ->
+  Composite hi lo f ns ->
+  Composite hi lo g ns ->
+  Composite hi lo h ns
+apply f (Single size lo x) (Single _ _ y) = Single size lo (f x y)
+apply f
+  (Prepend x0@(Single _ _ _) x1)
+  (Prepend y0@(Single _ _ _) y1) =
+    Prepend (apply f x0 y0) (apply f x1 y1)
+
+begin
+  :: forall size n f {psize}
+  . ( size ~ S psize
+    , Materialize size
+    )
+  => f n
+  -> Composite size (N2S 1) f '[ '(N2S 1,n) ]
+begin = Single (materialize @size) materialize
 
 pack
   :: forall hi lo ns n
@@ -80,7 +111,8 @@ pack
   => Composite hi lo (Const n) ns
   -> n
 pack (Prepend x xs) = pack x + pack xs
-pack (Single (Const n)) = makePart (part @hi @lo) n
+pack (Single @psize size lo (Const n)) =
+  makePart (part (add lo size) lo) n
 
 unpack
   :: forall hi lo ns f g n
@@ -89,40 +121,29 @@ unpack
   -> Composite hi lo f ns
   -> n
   -> Composite hi lo g ns
-unpack apply = flip \packed -> \case
-  Single @_ @_ @hi' @lo' n ->
-    Single $ apply n $ takePart (part @hi' @lo') packed
+unpack f = flip \packed -> \case
+  Single size lo n ->
+    Single size lo $ f n $ takePart (part size lo) packed
   Prepend x xs ->
-    Prepend (unpack apply x packed) (unpack apply xs packed)
-
-begin
-  :: forall size n f
-  . ( KnownNat size
-    , size - 1 + 1 ~ size
-    , Compare size 1 ~ 'GT
-    )
-  => f n
-  -> Composite size 1 f '[ '(size,n) ]
-begin = Single
+    Prepend
+       (unpack f x packed)
+       (unpack f xs packed)
 
 append
-  :: forall size n f hi0 lo0 ns y {lo1} {hi1}
-  . ( lo1 ~ hi0 + 1
-    , hi1 - lo1 + 1 ~ size
-
-    , KnownNat hi1
-    , KnownNat lo1
-    , KnownNat size
-
-    , Compare hi1 lo1 ~ 'GT
-    , Compare lo1 0 ~ 'GT
+  :: forall size n f hi0 lo0 ns y {psize}
+  . ( size ~ S psize
+    , Materialize hi0
+    , Materialize size
     )
   => f n
   -> Composite hi0 lo0 f (y:ns)
-  -> Composite hi1 lo0 f ( '(size,n):y:ns)
-append = Prepend . Single
+  -> Composite (hi0 :+: size) lo0 f ( '(S hi0,n):y:ns)
+append = Prepend . Single size (Succ hi0)
+  where
+    size = materialize @size
+    hi0 = materialize @hi0
 
--- -- * Part
+-- * Part
 
 data Part hi lo = MkPart
   { offset :: Int
@@ -168,46 +189,60 @@ makePart MkPart {offset, mask} n =
   (n `shiftL'` fromInteger (toEnum offset))
     `conj` fromInteger mask
 
-part :: forall hi lo.
-  ( KnownNat hi
-  , KnownNat lo
-  , Compare lo 0 ~ 'GT 
-  ) =>
-  Part hi lo
-part = MkPart {offset, sizeBits, mask}
+stoInt :: Nat n -> Int
+stoInt Zero = 0
+stoInt (Succ n) = 1 + stoInt n
+
+class Materialize n where
+  materialize :: Nat n
+instance Materialize Z where
+  materialize = Zero
+instance Materialize n => Materialize (S n) where
+  materialize = Succ (materialize @n)
+
+natToInt :: Nat n -> Int
+natToInt Zero = 0
+natToInt (Succ n) = 1 + natToInt n
+
+add :: Nat a -> Nat b -> Nat (a :+: b)
+add Zero b = b
+add (Succ a) b = Succ (add a b)
+
+part :: forall size {plo} {psize}.
+  Nat size ->
+  Nat (S plo) ->
+  Part (S plo :+: psize) (S plo)
+part size lo = MkPart {offset, sizeBits, mask}
   where
     mask = shiftL template offset
     template :: Integer
     template = 2^sizeBits - 1
     sizeBits = hi - offset
-    hi = fromEnum $ natVal @hi Proxy
-    offset = fromEnum (natVal @lo Proxy) - 1
+    hi = natToInt (lo `add` size)
+    offset = natToInt lo - 1
 
 newtype Iso a b = MkIso (a -> b, b -> a)
 
 -- newtype S f g a = MkS (f a (g a))
 
-type Msg f = Composite 16 1 f
-   '[ '(8,Uint8) -- 8 bits size
-    , '(4,Uint4) -- 4 bits size
-    , '(4,Uint4) -- 4 bits size
+type Msg f = Composite (N2S 16) (N2S 1) f
+   '[ '(N2S 9, Uint8) -- 8 bits size
+    , '(N2S 5, Uint4) -- 4 bits size
+    , '(N2S 1, Uint4) -- 4 bits size
     ]
-
-packMsg
-  :: Msg (Const Uint16)
-  -> Uint16
-packMsg = pack
 
 compositeNatTrans ::
   (forall a. f a -> g a) ->
   Composite hi lo f ns ->
   Composite hi lo g ns
-compositeNatTrans f (Single x) = Single (f x)
+compositeNatTrans f (Single size lo x) = Single size lo (f x)
 compositeNatTrans f (Prepend x xs) =
   Prepend (compositeNatTrans f x) (compositeNatTrans f xs)
 
 msgSchema :: Msg (Iso Uint16)
-msgSchema = undefined
+msgSchema = append (MkIso (castDefault,safeCast))
+  $ append (MkIso (castDefault,safeCast))
+  $ begin (MkIso (castDefault,safeCast))
 
 data N = S N | Z
 
@@ -215,59 +250,55 @@ type family NToNat (n :: N) where
   NToNat Z = 0
   NToNat (S n) = 1 + NToNat n
 
-apply :: forall f g h hi lo ns.
-  (forall a. f a -> g a -> h a) ->
-  Composite (NToNat hi) (NToNat lo) f ns ->
-  Composite (NToNat hi) (NToNat lo) g ns ->
-  Composite (NToNat hi) (NToNat lo) h ns
-apply f (Single x) (Single y) = Single (f x y)
-apply f (Prepend @hi1 @hi0 x0 x1) (Prepend @hi1' @hi0' y0 y1) =
-  q'
-  where
-  q' :: Composite hi lo h ns
-  q' = q
-  q :: hi1 ~ hi1' => hi0 ~ hi0' => Composite hi lo h ns
-  q = Prepend @hi1 @hi0
-    (apply f x0 y0)
-    (apply f x1 undefined)
-
-
+data Gt a b where
+  GtBase :: Gt (S a) Z
+  GtStep :: Gt a b -> Gt (S a) (S b) 
+  
+data Nat (n :: N) where
+  Zero :: Nat Z
+  Succ :: Nat n -> Nat (S n)
 
 putMessage ::
   (Uint8,Uint4,Uint4) ->
   Msg (Iso Uint16) ->
   Msg (Const Uint16)
-putMessage (a,b,c) = \case
-  Prepend (Single fa) x -> undefined
+putMessage (a,b,c) = apply f
+    $ append (Identity c)
+    $ append (Identity b)
+    $ begin (Identity a)
+  where
+    f (Identity x) (MkIso (_,from)) = Const $ from x
+
+_ = pack $ putMessage (10,20,30) msgSchema
 
 unpackMsg' :: forall
   . Msg (Iso Uint16)
   -> Uint16 -- integer to extract these pieces from
   -> (Uint8,Uint4,Uint4)
 unpackMsg' schema n =
-  case unpack (\(MkIso (to,_)) n -> Identity $ to n) schema n of
-    Prepend (Single (Identity n3))
+  case unpack (\(MkIso (to,_)) x -> Identity $ to x) schema n of
+    Prepend (Single _ _ (Identity n3))
       (Prepend
-        (Single (Identity n2))
-        (Single (Identity n1))) -> (n3,n2,n1)
+        (Single _ _ (Identity n2))
+        (Single _ _ (Identity n1))) -> (n3,n2,n1)
 
 
 unpackMsg :: forall a
-  . Composite 16 1 (Const a) -- 16 bits size in total
-   '[ '(8,Integer) -- 8 bits size
-    , '(4,Integer) -- 4 bits size
-    , '(4,Integer) -- 4 bits size
+  . Composite (N2S 16) (N2S 1) (Const a) -- 16 bits size in total
+   '[ '(N2S 9,Integer) -- 8 bits size
+    , '(N2S 5,Integer) -- 4 bits size
+    , '(N2S 1,Integer) -- 4 bits size
     ]
   -> Integer -- integer to extract these pieces from
   -> (Integer,Integer,Integer)
 unpackMsg schema n =
   case unpack (const Const) schema n of
-    Prepend (Single (Const n3))
+    Prepend (Single _ _ (Const n3))
       (Prepend
-        (Single (Const n2))
-        (Single (Const n1))) -> (n3,n2,n1)
+        (Single _ _ (Const n2))
+        (Single _ _ (Const n1))) -> (n3,n2,n1)
 
--- -- * Instances
+-- -- -- * Instances
 
 newtype CompositeF hi lo ns a = MkCompositeF
   { unCompositeF :: Composite hi lo (Const a) ns
@@ -281,14 +312,14 @@ withComposite f = unCompositeF . f . MkCompositeF
 
 instance Functor (CompositeF hi lo ns) where
   fmap f (MkCompositeF c) = MkCompositeF case c of
-    Single @u @b (Const x) -> Single @u @b (Const $ f x)
+    Single size lo (Const x) -> Single size lo (Const $ f x)
     Prepend a b -> Prepend
       (withComposite (fmap f) a)
       (withComposite (fmap f) b)
 
 instance Foldable (CompositeF hi lo xs) where
   foldMap f = unCompositeF >>> \case
-    Single (Const n) -> f n
+    Single _ _ (Const n) -> f n
     Prepend a b -> foldMap f (MkCompositeF a) <> foldMap f (MkCompositeF b)
 
 -- -- * Existentials
@@ -310,30 +341,30 @@ instance Foldable (CompositeF hi lo xs) where
 
 -- -- * Examples
 
-partJ  = part @8 @5
+-- partJ  = part @8 @5
 
-collectedJJ = fold
-  $ pure @[] <$> MkCompositeF unpackedJJ
+-- collectedJJ = fold
+--   $ pure @[] <$> MkCompositeF unpackedJJ
 
-unpackedJJ = unpack (const Const) jj (pack jj)
+-- unpackedJJ = unpack (const Const) jj (pack jj)
 
-jj = append @8 @Uint8 (n 251)
-  $ append @4 @Bool (n 13)
-  $ begin @4 @Int (n 7)
-  where
-    n = Const @Integer
+-- jj = append @8 @Uint8 (n 251)
+--   $ append @4 @Bool (n 13)
+--   $ begin @4 @Int (n 7)
+--   where
+--     n = Const @Integer
 
-k =
-  append @8 @Uint8 cu
-  $ append @4 @Bool cu
-  $ begin @4 @String cu
-  where
-    cu = Const ()
+-- k =
+--   append @8 @Uint8 cu
+--   $ append @4 @Bool cu
+--   $ begin @4 @String cu
+--   where
+--     cu = Const ()
 
-j = Single @_ @String @19 @17 $ Const @_  ()
+-- j = Single @_ @String @19 @17 $ Const @_  ()
 
---msgStructure :: Composite 16 0 (Const ())'[Uint16, Uint4, Bool]
-msgStructure = Prepend (Single @_ @String @19 @17 $ Const @_ @String ())
-  $ Prepend (Single @_ @String @16 @9 $ Const ())
-  $ Prepend (Single @_ @String @8 @5  $ Const ()) -- 0x11110000
-  $ Single @_ @Bool @4 @1 $ Const ()
+-- --msgStructure :: Composite 16 0 (Const ())'[Uint16, Uint4, Bool]
+-- msgStructure = Prepend (Single @_ @String @19 @17 $ Const @_ @String ())
+--   $ Prepend (Single @_ @String @16 @9 $ Const ())
+--   $ Prepend (Single @_ @String @8 @5  $ Const ()) -- 0x11110000
+--   $ Single @_ @Bool @4 @1 $ Const ()
